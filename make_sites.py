91e90ca8907896f49689f3e7554b767ce70bbb1c
Martin Krämer <MartinKraemer84@gmail.com>
2023-12-07 20:48:09 +0100

    Added decomposition of two digit numbers

--- make_sites.py
+++ make_sites.py
@@ -4,6 +4,7 @@ if __name__ == "__main__":
     site = Site.make_site(
         contexts=[
             ('plus_over_10.html', {'solution': False}),
+            ('number_deconstruction_two_digits.html', {'solution': False}),
             ('index.html', {'solution': True}),
             ],
         outpath='docs'
